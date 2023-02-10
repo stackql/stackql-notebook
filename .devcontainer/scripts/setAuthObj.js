1491296957ff3b166d34163f0110895e4ace4415
jeffreyaven <javen@avensolutions.com>
2023-02-10 13:47:54 +1100

    removed devcontainer

--- .devcontainer/scripts/setAuthObj.js
+++
@@ -1,5 +0,0 @@
-const fs = require('fs');
-const data = fs.readFileSync('/config/providerAuth.json',
-            {encoding:'utf8', flag:'r'});
-const jsonData = JSON.parse(data);
-console.log(JSON.stringify(jsonData));
